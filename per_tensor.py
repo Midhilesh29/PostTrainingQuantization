99904a24e0e592a5aed8064fa1452c52b331f15d
Midhilesh29 <midhileshgithub@gmail.com>
2020-05-12 23:03:47 +0530

    added new feature

---
+++ per_tensor.py
@@ -0,0 +1,24 @@
+import torch.quantization
+import torch.nn as nn
+from utils import evaluate,print_size_of_model
+
+def per_tensor_quantization(model,data_loader,num_calibration_batches = 10,criterion = nn.CrossEntropyLoss()):
+
+    model.eval()
+
+    model.qconfig = torch.quantization.default_qconfig
+
+    print("Coniguration of quantization:\n", model.qconfig)
+
+    torch.quantization.prepare(model, inplace=True)
+
+    evaluate(model, criterion, data_loader, neval_batches=num_calibration_batches)
+
+    torch.quantization.convert(model, inplace=True)
+    print('Post Training Quantization: Convert done')
+
+    print("Model size after per-tensor quantization:",print_size_of_model(model))
+
+    return model
+
+
