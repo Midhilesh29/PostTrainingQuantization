fb0a616d35637ff0dcb293d24dcb2eca07d40d7c
Midhilesh29 <midhileshgithub@gmail.com>
2020-05-14 14:39:16 +0530

    Added mobilenet

--- compare_models.py
+++ compare_models.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 from QAlexnet import QAlexnet
+from QMobilenetv2 import QMobilenetv2
 from per_channel import per_channel_quantization
 from per_tensor import per_tensor_quantization
 from utils import evaluate,print_size_of_model
@@ -17,7 +18,10 @@ def compare_model(model_name,num_eval_batches=10,criterion = nn.CrossEntropyLoss
     per_channel_model=None
     if(model_name=="AlexNet"):
         model=QAlexnet()
-
+    elif(model_name=="MobileNetv2"):
+        model=QMobilenetv2()
+    print("Checking size of "+f"{model_name}")
+    print("Size (MB) of "+f"{model_name} is:",print_size_of_model(model))
     print("Evaluvating "+ f"{model_name} on the imagenet data\n")
     _,model_top5 = evaluate(model, criterion, data_loader, neval_batches=num_eval_batches)
 
