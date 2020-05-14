202059d77c90d421d5ffb839dae8b6d3420b4be6
Midhilesh29 <midhileshgithub@gmail.com>
2020-05-14 14:16:51 +0530

    added generalization

--- compare_models.py
+++ compare_models.py
@@ -12,26 +12,30 @@ print("Data loaders are created")
 
 def compare_model(model_name,num_eval_batches=10,criterion = nn.CrossEntropyLoss()):
 
+    model=None
+    per_tensor_model=None
+    per_channel_model=None
     if(model_name=="AlexNet"):
-        Alexmodel=QAlexnet()
-        print("Evaluvating alexnet model on the imagenet data\n")
-        _,model_top5 = evaluate(Alexmodel, criterion, data_loader, neval_batches=num_eval_batches)
+        model=QAlexnet()
 
-        per_tensor_model = per_tensor_quantization(model=Alexmodel,criterion = nn.CrossEntropyLoss(),data_loader=data_loader)
-        print("Checking model size after per_tensor quantization")
-        print("Print per-tensor quantized model size (MB):\n",print_size_of_model(per_tensor_model))
-        print("Evaluvating the per-tensor model on the imagenet data\n")
-        _,tensor_top5 = evaluate(per_tensor_model, criterion, data_loader, neval_batches=num_eval_batches)
+    print("Evaluvating "+ f"{model_name} on the imagenet data\n")
+    _,model_top5 = evaluate(model, criterion, data_loader, neval_batches=num_eval_batches)
 
-        per_channel_model = per_channel_quantization(model=Alexmodel,criterion = nn.CrossEntropyLoss(),data_loader=data_loader)
-        print("Checking model size after per_tensor quantization")
-        print("Print per-channel quantized model size (MB):\n",print_size_of_model(per_tensor_model))
-        print("Evaluvating the per-tensor model on the imagenet data\n")
-        _,channel_top5 = evaluate(per_channel_model, criterion, data_loader_test, neval_batches=num_eval_batches)
+    per_tensor_model = per_tensor_quantization(model=model,criterion = nn.CrossEntropyLoss(),data_loader=data_loader)
+    print("Checking model size after per_tensor quantization")
+    print("Print per-tensor quantized model size (MB):\n",print_size_of_model(per_tensor_model))
+    print("Evaluvating the per-tensor model on the imagenet data\n")
+    _,tensor_top5 = evaluate(per_tensor_model, criterion, data_loader, neval_batches=num_eval_batches)
 
-        print("\n")
-        print(f"{model_name} per-tensor quantized top5 accuracy on imagenet data with {str(num_eval_batches)} batches is {str(tensor_top5.avg)}")
-        print("\n")
-        print(f"{model_name} per-chennal quantized top5 accuracy on imagenet data with {str(num_eval_batches)} batches is {str(channel_top5.avg)}")
-        print("\n")
-        print(f"{model_name} top5 accuracy on imagenet data with {str(num_eval_batches)} batches {str(model_top5.avg)}")
+    per_channel_model = per_channel_quantization(model=model,criterion = nn.CrossEntropyLoss(),data_loader=data_loader)
+    print("Checking model size after per_tensor quantization")
+    print("Print per-channel quantized model size (MB):\n",print_size_of_model(per_tensor_model))
+    print("Evaluvating the per-tensor model on the imagenet data\n")
+    _,channel_top5 = evaluate(per_channel_model, criterion, data_loader_test, neval_batches=num_eval_batches)
+
+    print("\n")
+    print(f"{model_name} per-tensor quantized top5 accuracy on imagenet data with {str(num_eval_batches)} batches is {str(tensor_top5.avg)}")
+    print("\n")
+    print(f"{model_name} per-chennal quantized top5 accuracy on imagenet data with {str(num_eval_batches)} batches is {str(channel_top5.avg)}")
+    print("\n")
+    print(f"{model_name} top5 accuracy on imagenet data with {str(num_eval_batches)} batches {str(model_top5.avg)}")
