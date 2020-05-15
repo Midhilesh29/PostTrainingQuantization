f3b3fe212ce9029b2d26182c6f82bb301795024f
Midhilesh29 <midhileshgithub@gmail.com>
2020-05-15 17:46:30 +0530

    bug fixed

--- compare_models.py
+++ compare_models.py
@@ -46,19 +46,22 @@ def compare_model(deep_learning_models,num_eval_batches=10,criterion = nn.CrossE
             break
 
         print("Checking size of "+f"{model_name}")
-        print("Size (MB) of "+f"{model_name} is:",print_size_of_model(model))
+        size_of_model = print_size_of_model(model)
+        print("Size (MB) of "+f"{model_name} is:",)
         print("Evaluvating "+ f"{model_name} on the imagenet data\n")
         _,model_top5 = evaluate(model, criterion, data_loader, neval_batches=num_eval_batches)
 
         per_tensor_model = per_tensor_quantization(model=model,criterion = nn.CrossEntropyLoss(),data_loader=data_loader)
         print("Checking model size after per_tensor quantization")
-        print("Print per-tensor quantized model size (MB):\n",print_size_of_model(per_tensor_model))
+        tensor_size = print_size_of_model(per_tensor_model)
+        print("Print per-tensor quantized model size (MB):\n",tensor_size)
         print("Evaluvating the per-tensor model on the imagenet data\n")
         _,tensor_top5 = evaluate(per_tensor_model, criterion, data_loader, neval_batches=num_eval_batches)
 
         per_channel_model = per_channel_quantization(model=model,criterion = nn.CrossEntropyLoss(),data_loader=data_loader)
         print("Checking model size after per_tensor quantization")
-        print("Print per-channel quantized model size (MB):\n",print_size_of_model(per_tensor_model))
+        channel_size = print_size_of_model(per_channel_model)
+        print("Print per-channel quantized model size (MB):\n",channel_size)
         print("Evaluvating the per-tensor model on the imagenet data\n")
         _,channel_top5 = evaluate(per_channel_model, criterion, data_loader_test, neval_batches=num_eval_batches)
 
@@ -68,14 +71,12 @@ def compare_model(deep_learning_models,num_eval_batches=10,criterion = nn.CrossE
         print(f"{model_name} per-channel quantized top5 accuracy on imagenet data with {str(num_eval_batches)} batches is {str(channel_top5.avg)}")
         print("\n")
         print(f"{model_name} top5 accuracy on imagenet data with {str(num_eval_batches)} batches {str(model_top5.avg)}")
+        print("\n")
 
-        tensor_size=print_size_of_model(per_tensor_model)
-        channel_size=print_size_of_model(per_channel_model)
         tensor_accuracy=tensor_top5.avg
         channel_accuracy=channel_top5.avg
-        size_of_model = print_size_of_model(model)
         accuracy_of_model = model_top5.avg
-        statistics[model_name]={"Parent_model_size":size_of_model,
+        statistics[model_name]={"parent_model_size":size_of_model,
                                 "parent_top5_accuracy":accuracy_of_model,
                                 "tensor_quantized_size":tensor_size,
                                 "tensor_quantized_top5_accuracy":tensor_accuracy,
